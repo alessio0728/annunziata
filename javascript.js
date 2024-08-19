20ea063e37550ea4b232cd00dd98ecd3f43597ea
Alessio Giannoccaro <mrbigalessio@gmail.com>
2024-08-19 22:24:33 +0200

    SALVA DEL 19-08-2024 ore 22.24

--- javascript.js
+++ javascript.js
@@ -1,1 +1,29 @@
 
+
+    document.addEventListener('DOMContentLoaded', function() {
+        const grid = document.querySelector('.grid');
+        const col33Elements = document.querySelectorAll('.col-33');
+
+        col33Elements.forEach(function(col33) {
+            col33.addEventListener('click', function() {
+                if (grid.style.animationPlayState === 'paused') {
+                    grid.style.animationPlayState = 'running';
+                } else {
+                    grid.style.animationPlayState = 'paused';
+                }
+            });
+        });
+    });
+
+
+    //MARCHETTI NON FUNZIONA//
+
+    ScrollReveal().reveal('.reveal', { distance: '50px' });
+
+     
+  
+   
+   
+   
+   
+
